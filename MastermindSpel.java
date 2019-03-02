773987721fe9fd4bd46f6b0e37ab37e28c1397ad
EJoling <emieljoling@gmail.com>
2019-03-02 16:10:42 +0100

    tm correcte invoer werkt, nu code vergelijken

--- MastermindSpel.java
+++ MastermindSpel.java
@@ -4,9 +4,11 @@ import java.util.Random;
 public class MastermindSpel{
 		
 	static  String code = "";
-//	static String letterInCode = "";
+	static int letterInCode[] = {0,0,0,0};
 	static int aantalBeurten = 12;
 	static Speler speler;
+	static boolean codeGecheckt[] = new boolean[4];
+	static boolean gokGecheckt[] = new boolean[4];
 	
 //	constructor geeft bij elk nieuw spel een code
 		MastermindSpel() {
@@ -70,45 +72,62 @@ public class MastermindSpel{
 				
 				
 			//	als de code 4 letters heeft én het zijn letters van de invoer die mag dan wordt de code gecontroleerd
-				if(invoerGeldig == true && codeGok.charAt(0) != 'q') {
-					System.out.println("De code wordt gecheckt. 0: letter op de juiste plaats; +: letter zit wel in de code maar niet op deze plaats; X: letter zit niet in de code");
-				}//end if
+			//	if(invoerGeldig == true && codeGok.charAt(0) != 'q') {
+			//		System.out.println("De code wordt gecheckt. 0: letter op de juiste plaats; +: letter zit wel in de code maar niet op deze plaats; X: letter zit niet in de code");
+			//	}//end if
 	}//end invoerControleren
 
+	
+	
+	
+	
+	
 	// Controle op gokCheck: zitten de letters erin op de juiste plek, erin op een andere plek of er niet in
 	static void gokCheck() {
 		if(Speler.gestopt != true) {
+			System.out.println("De code wordt gecheckt. 0: letter op de juiste plaats; +: letter zit wel in de code maar niet op deze plaats; X: letter zit niet in de code");
+			
+			
+			MastermindSpel.codeGecheckt = new boolean [4];
+			MastermindSpel.gokGecheckt = new boolean [4];
 			for (int i=0; i<Speler.spelerInvoer.length();i++) {
-				System.out.println("gokCheck in Mm");
 				checkLetterOpGoedePlek(i);
 				checkLetterAanwezig(i);
-				checkLetterAfwezig(i);
+				//checkLetterAfwezig(i);			
 			}//end for loop
-	//	System.out.println("	 lettersInCode:"+MastermindSpel.letterInCode);
+		
+//			for loop om te testen
+			System.out.println("");
+			for(int i=0;i<4;i++) {
+				System.out.println("CodeGecheckt: " + codeGecheckt[i]);
+				}//end for print
+			System.out.println("");
+			
 		aantalBeurten--;
-		System.out.println("");
+		
 		System.out.println("Dit was beurt " + (12-aantalBeurten) + ". Je hebt nog "+aantalBeurten+" beurten om de juiste code te raden.");
 		}//end if
+		
+	
 	}//end methode gokCheck
 	
 	
 	// Checkt of de letter in spelerInvoer op dezelfde plek staat als in de code
 	static void checkLetterOpGoedePlek(int i) {
-		for(int j =0; j < Speler.spelerInvoer.length(); j++) {
 			if (Speler.spelerInvoer.charAt(i) == MastermindSpel.code.charAt(i)) {
 				System.out.print("0");
-				//letterInCode += Speler.spelerInvoer.charAt(i);
-				break;
+				codeGecheckt[i]= gokGecheckt[i] = true;
 			}//end if
-		}//end for
 	}//end methode checkLetterOpGoedePlek
 
 	// Checkt of de letter in spelerInvoer wel in de code staat, maar niet op dezelfde plek
 	static void checkLetterAanwezig(int i) {
 		for(int j =0; j < Speler.spelerInvoer.length(); j++) {
-			if (Speler.spelerInvoer.charAt(i) != MastermindSpel.code.charAt(i) 
-					&& Speler.spelerInvoer.charAt(i) == MastermindSpel.code.charAt(j)){
+			if (!codeGecheckt [i] &&!gokGecheckt[i]
+					&& Speler.spelerInvoer.charAt(i) == MastermindSpel.code.charAt(j)
+					){
 				System.out.print("+");
+				codeGecheckt[i]= gokGecheckt[j] = true;
 				break;
 			}//end if
 		}//end for
@@ -118,15 +137,17 @@ public class MastermindSpel{
 	static void checkLetterAfwezig(int i) {
 		for(int j =0; j < Speler.spelerInvoer.length(); j++) {
 			if (Speler.spelerInvoer.charAt(i) != MastermindSpel.code.charAt(j)
+					&& MastermindSpel.letterInCode[j]!=1
+					&& MastermindSpel.letterInCode[j]!=5
+					
 					//&& Speler.spelerInvoer.charAt(i) != MastermindSpel.code.charAt(j)
 					){
 				System.out.print("X");
+				MastermindSpel.letterInCode[i]= 0;
 				break;
 			}// end if
 		}//end for
 	}//end methode checkLetterAfwezig
-	
-	
 	
 	
 
